de79995d53ed49d6974968ebcf1e6c590a04210f
Zabeebzaliz <szabeebzaliz7@gmail.com>
2026-01-20 14:52:56 +0530

    Update STATIC_ROOT and comment out media settings in Django settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,11 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/ubuntu/portfolio/collectstatic/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
